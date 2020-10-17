35e54a09162454d60bee6dadf788db684860a776
EshanDev <theerapong.b@ru.ac.th>
2020-10-17 10:02:48 +0700

    edit new content

--- resources/views/auth/partials/register.blade.php
+++ resources/views/auth/partials/register.blade.php
@@ -2,7 +2,7 @@
     <div class="content">
         <div class="content-header">
             <div class="group-text">
-                <div>กรอกข้อมูลนักศึกษา</div>
+                <div>ฟอร์มลงทะเบียน</div>
             </div>
         </div>
 
@@ -27,10 +27,10 @@
 
                 <div class="group-form">
                     <fieldset class="border rounded custom-fieldset">
-                        <legend class="w-auto legend">ส่วนที่ 1 รหัสยืนยันรหัสลงทะเบียน</legend>
+                        <legend class="w-auto legend">ส่วนที่ 1 ข้อมูลรหัสลงทะเบียน</legend>
                         <div class="grid-container">
-                            <div class="form-group">
-                                <label for="registration_code">ยืนยันรหัสลงทะเบียน</label>
+                            <div class="form-group code">
+                                <label for="registration_code">รหัสลงทะเบียน</label>
                                 <input type="text" class="form-control serials" name="registration_code"
                                        id="registration_code" placeholder="ABCDE-ABCDE-ABCDE-ABCDE"
                                        value=""
@@ -41,14 +41,19 @@
                             <div class="form-group">
                                 <label for="student_code">รหัสนักศึกษา</label>
                                 <input type="text" name="student_code" class="form-control"
-                                       value="" placeholder="ระบุรหัสนักศึกษา">
+                                       value="" placeholder="ระบุรหัสนักศึกษา"
+                                       onfocus="this.placeholder = ''"
+                                       onblur="this.placeholder = 'ระบุรหัสนักศึกษา'">
 
                             </div>
 
                             <div class="form-group">
                                 <label for="student_email">ที่อยู่อีเมล์</label>
                                 <input type="email" name="student_email" class="form-control"
-                                       value="" placeholder="ระบุที่อยู่อีเมล์">
+                                       value=""
+                                       placeholder="ระบุที่อยู่อีเมล์"
+                                       onfocus="this.placeholder = ''"
+                                       onblur="this.placeholder = 'ระบุที่อยู่อีเมล์'">
 
                             </div>
                         </div>
@@ -67,18 +72,25 @@
                             <div class="form-group">
                                 <label for="student_name">ชื่อ/สกุล</label>
                                 <input type="text" name="student_name" class="form-control"
-                                       value="{{ old('student_name') }}" placeholder="ชื่อ - นามสกุล">
+                                       value="{{ old('student_name') }}"
+                                       placeholder="ชื่อ - นามสกุล"
+                                       onblur="this.placeholder = 'ชื่อ - นามสกุล'"
+                                       onfocus="this.placeholder =''">
                             </div>
                             <div class="form-group">
                                 <label for="student_branch">สาขาวิชาเอก</label>
                                 <input type="text" name="student_branch" value="{{ old('student_branch') }}"
-                                       class="form-control" placeholder="สาขาวิชาที่กำลังศึกษา">
+                                       class="form-control"
+                                       placeholder="สาขาวิชาที่กำลังศึกษา"
+                                       onfocus="this.placeholder =''"
+                                       onblur="this.placeholder= 'สาขาวิชาที่กำลังศึกษา'">
                             </div>
 
                             <div class="form-group">
                                 <label for="student_faculty">คณะ</label>
                                 <input type="text" class="form-control" name="student_faculty"
-                                       value="{{ old('student_faculty') }}" placeholder="คณะที่กำลังศึกษา">
+                                       value="{{ old('student_faculty') }}" placeholder="คณะที่กำลังศึกษา"
+                                       onblur="this.placeholder='คณะที่กำลังศึกษา'" onfocus="this.placeholder =''">
                             </div>
 
                         </div>
@@ -97,16 +109,19 @@
                             <div class="form-group">
                                 <label for="name">รหัสผู้ใช้งาน <span class="text-danger">***เปลี่ยนได้</span></label>
                                 <input type="text" name="name" class="form-control" value=""
-                                       placeholder="กำหนดรหัสผู้ใช้งาน">
+                                       placeholder="กำหนดรหัสผู้ใช้งาน" onfocus="this.placeholder=''"
+                                       onblur="this.placeholder='กำหนดรหัสผู้ใช้งาน'">
                             </div>
                             <div class="form-group">
                                 <label for="password">รหัสผ่าน</label>
                                 <input type="password" name="password" id="password" placeholder="กำหนดรหัสผ่านใหม่"
+                                       onblur="this.placeholder='กำหนดรหัสผ่านใหม่'" onfocus="this.placeholder=''"
                                        class="form-control">
                             </div>
                             <div class="form-group">
                                 <label for="password-confirm">ยืนยันรหัสผ่าน</label>
                                 <input type="password" name="password_confirmation" placeholder="ยืนยันรหัสผ่านอีกครั้ง"
+                                       onfocus="this.placeholder=''" onblur="this.placeholder='ยืนยันรหัสผ่านอีกครั้ง'"
                                        class="form-control">
                             </div>
                         </div>
@@ -117,7 +132,7 @@
                 <!-- End Form Group Three -->
 
                 <div class="register-btn">
-                    <input type="submit" class="btn btn-secondary btn-lg" value="ส่งข้อมูล">
+                    <input type="submit" class="btn btn-primary" value="ส่งข้อมูล">
                 </div>
 
             </form>
