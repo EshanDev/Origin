c1000f7b460207e188c462b756268a9aa7355014
EshanDev <theerapong.b@ru.ac.th>
2020-10-20 14:38:04 +0700

    Add/Edit 404.scss

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>404 Page not found</title>
     <link rel="stylesheet" href="{{ mix('css/ui.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome/css/all.css') }}">
 </head>
 <body id="page_not_found">
 
     <div class="content">
+        <div class="icons text-center"><i class="fal fa-exclamation-triangle"></i></div>
         <div class="text-header">404 page not found</div>
         <div class="text-fallback">
             <a href="{{ route('auth.home') }}">GoBack</a>
